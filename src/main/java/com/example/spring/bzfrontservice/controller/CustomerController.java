be21db54957c6b3d0b6e9dadfa62a30754543d10
Ninky0 <2020113392@dgu.ac.kr>
2025-02-21 23:46:21 +0900

    refactor : purchase controller

--- src/main/java/com/example/spring/bzfrontservice/controller/CustomerController.java
+++ src/main/java/com/example/spring/bzfrontservice/controller/CustomerController.java
@@ -24,7 +24,6 @@ public class CustomerController {
     private final UserService userService;
     private final SellerService sellerService;
     private final CustomerService customerService;
-    private final PurchaseService purchaseService;
     private final CongdongService congdongService;
 
     // 상품 상세 페이지
@@ -54,93 +53,6 @@ public class CustomerController {
         return "cart";
     }
 
-    // 바로 구매
-    @GetMapping("/purchase/direct")
-    public String showPaymentPage(@RequestParam("productId") Long productId,
-                                  @RequestParam("price") Double price,
-                                  @RequestParam("memberNo") Long memberNo,
-                                  @RequestParam("quantity") Integer quantity,
-                                  @RequestParam("congId") Long congId,
-                                  Model model) {
-        SecurityUserDTO dto = userService.loadMemberDetail(memberNo);
-
-        // 필요한 데이터를 모델에 추가
-        model.addAttribute("productId", productId);
-        model.addAttribute("price", price);
-        model.addAttribute("quantity", quantity);
-        model.addAttribute("totalPrice", quantity * price);
-        model.addAttribute("member", dto);
-        model.addAttribute("congId", congId);
-
-        return "payment";
-    }
-
-    // 주문 요청 처리
-    @GetMapping("/purchase/cart")
-    public String processCartPurchase( @RequestParam(value = "orderId", required = false) String orderId,
-                                       @RequestParam("totalAmount") Double totalAmount,
-                                      @RequestParam("productList") String productListJson,
-                                      @RequestParam("memberNo") Long memberNo,
-                                      Model model) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        List<Map<String, Object>> productList;
-        try {
-            productList = objectMapper.readValue(productListJson, new TypeReference<List<Map<String, Object>>>() {});
-            System.out.println("productListJson: " + productListJson + ", productList: " + productList);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Invalid productList format", e);
-        }
-
-        System.out.println("Total Amount: " + totalAmount);
-        System.out.println("Product List: " + productList);
-        System.out.println("Member No: " + memberNo);
-
-        SecurityUserDTO dto = userService.loadMemberDetail(memberNo);
-
-        // 모델에 데이터 추가
-        model.addAttribute("totalPrice", totalAmount);
-        model.addAttribute("productList", productList);
-        model.addAttribute("member", dto);
-
-        return "payment2";
-    }
-
-    @GetMapping("/purchase/success")
-    public String successPayment(@RequestParam String paymentKey,
-                                 @RequestParam String orderId,
-                                 @RequestParam String amount,
-                                 @RequestParam String paymentType){
-
-
-        return "pay_success";
-    }
-
-    @GetMapping("/purchase/fail")
-    public String failPayment(@RequestParam String code,
-                              @RequestParam String message,
-                              @RequestParam String orderId){
-        return "pay_fail";
-    }
-
-    @GetMapping("/history")
-    public String history(
-            @RequestParam Long memberNo,
-            @RequestParam(defaultValue = "1") int page, // 기본값을 1로 설정
-            @RequestParam(defaultValue = "5") int size,
-            Model model) {
-
-        Page<PurchaseDTO> purchasePage = purchaseService.getPurchaseListByMemberNo(memberNo, page, size);
-
-        List<PurchaseDTO> purchases = purchasePage.getContent(); // 현재 페이지의 데이터만 가져옴
-        purchaseService.enrichPurchasesWithProducts(purchases); // ✅ 상품 정보 추가
-
-        model.addAttribute("purchases", purchases); // ✅ 상품 정보가 포함된 purchases 추가
-        model.addAttribute("currentPage", page);
-        model.addAttribute("totalPages", purchasePage.getTotalPages());
-        model.addAttribute("memberNo", memberNo);
-
-        return "purchase_list";
-    }
 
 
     @GetMapping("/history/review/{productId}/{purchaseId}")
